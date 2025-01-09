d17d664c20a7a3732c35dcc2d0b9d9524f12630a
PuttiCristian <cristianputti@gmail.com>
2025-01-09 10:35:12 +0100

    primo commit

---
+++ src/putti_giocosedie/Posto.java
@@ -0,0 +1,24 @@
+package putti_giocosedie;
+
+public class Posto
+
+{
+	private boolean occupato;
+
+	public Posto()
+
+	{
+		occupato = false;
+	}
+
+	public synchronized boolean libero() {
+		return (!occupato);
+	}
+
+	public synchronized boolean occupa() {
+		if (occupato)
+			return false;
+		else
+			return occupato = true;
+	}
+}
